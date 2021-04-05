ad0fbf0ba6253a1615705b90a3ca6a08f591588d
hmoon630 <s19017@gsm.hs.kr>
2021-04-05 21:11:34 +0900

    Improve Two Sum

--- solutions/1-TwoSum.py
+++ solutions/1-TwoSum.py
@@ -2,7 +2,8 @@
 from typing import List
 from itertools import combinations
 
-class Solution:
+# Brute Force
+class SolutionOld:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         combs = list(combinations(nums, 2))
 
@@ -10,4 +11,13 @@ class Solution:
             if sum(comb) == target:
                 return [nums.index(comb[0]), nums.index(comb[1], nums.index(comb[0]) + 1)]
 
-print(Solution().twoSum(nums = [3,3], target = 6))
+# Hash Map
+class Solution:
+    def twoSum(self, nums:List[int], target: int) -> List[int]:
+        dic = dict([(num, index) for index, num in enumerate(nums)])
+
+        for index, num in enumerate(nums):
+            if target - num in dic and index != dic[target - num]:
+                return [index, dic[target - num]]
+
+print(Solution().twoSum(nums = [3, 3], target = 6))
