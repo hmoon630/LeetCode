1aba515127ec85f2dcd43f6415843d4580834832
hmoon630 <s19017@gsm.hs.kr>
2021-04-09 11:55:20 +0900

    Regular Expression Matching

---
+++ solutions/10-RegularExpressionMatching.py
@@ -0,0 +1,15 @@
+# https://leetcode.com/problems/regular-expression-matching
+
+import re 
+
+class Solution:
+    def isMatch(self, s: str, p: str) -> bool:
+        result = re.match(p, s)
+        if result:
+            return result.group() == s
+        else:
+            return False
+
+s = "aa"
+p = "a"
+print(Solution().isMatch(s, p))
